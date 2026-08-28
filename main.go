62da1a817d78ac8b12b9762efdfadbd5ae9dcfd3
krehwell <fahrimuham001@gmail.com>
2026-08-28 12:28:01 +0700

    21 like why not

--- main.go
+++ main.go
@@ -75,15 +75,6 @@ var hashes = make(
 	map[string]map[string]string,
 ) // { "user:1": { "name": "alice", "email": "a@mail.com" }, "user:2", { "name": "bob", "age": "30" } }
 
-func contains(slice []string, target string) bool {
-	for _, item := range slice {
-		if item == target {
-			return true
-		}
-	}
-	return false
-}
-
 type QueuedCmd struct {
 	cmd  string
 	args []string
@@ -95,6 +86,15 @@ type ClientState struct {
 }
 
 func (c *ClientState) Dispatch(cmd string, args ...string) string {
+	contains := func(slice []string, target string) bool {
+		for _, item := range slice {
+			if item == target {
+				return true
+			}
+		}
+		return false
+	}
+
 	if c.isInMulti && !contains([]string{"EXEC", "DISCARD", "MULTI"}, cmd) {
 		c.queued = append(c.queued, QueuedCmd{cmd, args})
 		return encodeSimpleString("QUEUED")
